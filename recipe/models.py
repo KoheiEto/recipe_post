f2290c0ace706a1f6ca25517c08d9d49d4738d9e
etokohei <kohei.eto@rakus-partners.co.jp>
2021-05-17 15:17:29 +0900

    Please enter the commit message for your changes. Lines starting

---
+++ recipe/models.py
@@ -0,0 +1,16 @@
+from enum import auto
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+    # タイトル
+    title = models.CharField(max_length=200)
+    # 内容
+    content = models.TextField()
+    # 作成日
+    created = models.DateTimeField(auto_now_add=True)
+    # 最終更新日
+    modified = models.DateTimeField(auto_now=True)
+
+# データベースにデータ反映する＝マイグレーション
+# ssss
