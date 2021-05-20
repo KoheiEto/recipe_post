3a7f071525ccb45316761c1737ec55285f45123e
etokohei <kohei.eto@rakus-partners.co.jp>
2021-05-20 20:10:04 +0900

    モデルにフィールド追加、マイグレーションファイル作成、マイグレーション

--- recipe/models.py
+++ recipe/models.py
@@ -7,6 +7,8 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     # 内容
     content = models.TextField()
+    # 特徴
+    descripton = models.TextField(blank=True, default="")
     # 作成日
     created = models.DateTimeField(auto_now_add=True)
     # 最終更新日
