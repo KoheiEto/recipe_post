95f5441dbcc005f37468ad19b4926555a93c68a6
etokohei <kohei.eto@rakus-partners.co.jp>
2021-05-29 08:47:37 +0900

    詳細表示用のURLを設定

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from lib.views import IndexTemplateView
-from recipe.views import RecipeListView, RecipeCreateView
+from recipe.views import RecipeListView, RecipeCreateView, RecipeDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipe/', RecipeListView.as_view(), name="recipe-index"),
     path('recipe/create', RecipeCreateView.as_view(), name="recipe-create"),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail"),
     path('', IndexTemplateView.as_view(), name="index"),
 ]
