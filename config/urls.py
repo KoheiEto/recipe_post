980b03432751f00c2bd5ae80abd378e00d5442fe
etokohei <kohei.eto@rakus-partners.co.jp>
2021-05-29 07:23:30 +0900

    URL設定

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from lib.views import IndexTemplateView
-from recipe.views import RecipeListView
+from recipe.views import RecipeListView, RecipeCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipe/', RecipeListView.as_view(), name="recipe-index"),
+    path('recipe/create', RecipeCreateView.as_view(), name="recipe-create"),
     path('', IndexTemplateView.as_view(), name="index"),
 ]
--- recipe/models.py
+++ recipe/models.py
@@ -1,6 +1,7 @@
 from enum import auto
 from django.db import models
 
+
 # Create your models here.
 class Recipe(models.Model):
     # タイトル
@@ -8,10 +9,11 @@ class Recipe(models.Model):
     # 内容
     content = models.TextField()
     # 特徴
-    descripton = models.TextField(blank=True, default="")
+    description = models.TextField(blank=True, default="")
     # 作成日
     created = models.DateTimeField(auto_now_add=True)
     # 最終更新日
     modified = models.DateTimeField(auto_now=True)
+
 
 # データベースにデータ反映する＝マイグレーション
