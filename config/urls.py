a546d7198404bd417dd1a04d2588e564a7ae3ccc
etokohei <kohei.eto@rakus-partners.co.jp>
2021-06-17 19:14:50 +0900

    ユーザアプリケーション作成

--- config/urls.py
+++ config/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipe/', include("recipe.urls")),
     path('comment/', include("comment.urls", namespace="comment")),
+    path('staffroom/', include("staffroom.urls", namespace="staffroom")),
     path('', IndexTemplateView.as_view(), name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
