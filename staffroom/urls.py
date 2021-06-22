dc5b0582c421bc7ec9e7196dce9dd7af34884669
etokohei <kohei.eto@rakus-partners.co.jp>
2021-06-22 10:32:37 +0900

    ユーザページテンプレート作成

--- staffroom/urls.py
+++ staffroom/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import StaffroomTemplateView
+
 app_name = "staffroom"
 
-urlpatterns = []
+urlpatterns = [
+    path("", StaffroomTemplateView.as_view(), name="index"),
+]
