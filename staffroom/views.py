9ba7faf4d08d1c3b9cfe181d93218b3b8437265a
etokohei <kohei.eto@rakus-partners.co.jp>
2021-06-22 10:35:36 +0900

    ユーザが投稿したレシピを一覧表示

--- staffroom/views.py
+++ staffroom/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from recipe.models import Recipe
 
 
 class StaffroomTemplateView(TemplateView):
     template_name = "staffroom/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context['recipe_list'] = Recipe.objects.filter(
+                user=self.request.user)
+        else:
+            context['recipe_list'] = None
+
+        return context
