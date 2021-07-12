57336632769d1e1152dae672e58bb8c8b0a4a0a4
etokohei <kohei.eto@rakus-partners.co.jp>
2021-07-12 13:11:59 +0900

    ユーザ機能

--- staffroom/views.py
+++ staffroom/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, DeleteView, UpdateView
+from django.urls import reverse, reverse_lazy
+from django.contrib import messages
 from recipe.models import Recipe
+from recipe.forms import RecipeForm
 
 
 class StaffroomTemplateView(TemplateView):
@@ -18,3 +21,54 @@ class StaffroomTemplateView(TemplateView):
             context['recipe_list'] = None
 
         return context
+
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    success_url = reverse_lazy("recipe:index")
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.POST.dict()
+        data['user'] = user.id
+        form = RecipeForm(data=data, files=request.FILES)
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        messages.success(self.request, "保存しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "保存できませんでした")
+        return super().form_invalid(form)
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    fields = ["title", "content", "description", "image"]
+
+    def get_success_url(self):
+        pk = self.kwargs.get("pk")
+        return reverse("recipe:detail", kwargs={"pk": pk})
+
+    def form_valid(self, form):
+        messages.success(self.request, "更新しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新できませんでした")
+        return super().form_invalid(form)
+
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    success_url = reverse_lazy("recipe:index")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "削除しました")
+        return super().delete(request, *args, **kwargs)
