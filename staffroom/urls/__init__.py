51b0d71e5fb5fd4f3dd0943a9e677710590b259e
etokohei <kohei.eto@rakus-partners.co.jp>
2021-07-12 15:34:29 +0900

    bugfix

--- staffroom/urls/__init__.py
+++ staffroom/urls/__init__.py
@@ -7,4 +7,5 @@ app_name = "staffroom"
 urlpatterns = [
     path("", StaffroomTemplateView.as_view(), name="index"),
     path("recipe/", include("staffroom.urls.recipe", namespace="recipe")),
+    
 ]
