d1eb8f1419702e089c4b29aaa8b7104b2dea229d
etokohei <kohei.eto@rakus-partners.co.jp>
2021-07-12 13:55:45 +0900

    ログイン機能

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django import urls
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.urls.conf import include
 from lib.views import IndexTemplateView
@@ -29,5 +29,8 @@ urlpatterns = [
     path('comment/', include("comment.urls", namespace="comment")),
     path('staffroom/', include("staffroom.urls", namespace="staffroom")),
     path('login', LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout',
+         LogoutView.as_view(template_name="logout.html"),
+         name="logout"),
     path('', IndexTemplateView.as_view(), name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
